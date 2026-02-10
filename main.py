5c3f2ef98368baa794baa3b4e5747fadd9029e34
SA-9R-Mandia <mandia.ke@obmontessori.edu.ph>
2026-02-10 12:11:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyscript import document
+from pyscript import display, document
 
 def intrcheck(e):
     document.getElementById('output').innerHTML = ' '
@@ -6,23 +6,33 @@ def intrcheck(e):
     
     registration = document.querySelector('input[name="registration"]:checked').value
     clearance = document.querySelector('input[name="clear"]:checked').value
-    grade_level = int(document.getElementById('level').value)
-    section = document.getElementById('section').value
+    grade_level = document.getElementById('level').value
+    
 
-    if registration != 'registered':
+    if registration != 'regis':
         display(f'Not eligible: student is not registered for Intrams. Please ask your PE teacher about the online registraton.', target='output')
-    elif clearance != 'cleared':
+    elif clearance != 'yesclear':
         display(f'Not eligible: medical clearance required. Get your clearance at the clinic.', target='output')
-    elif section == 'emerald' and grade_level == 8 or 9:
+    elif grade_level == 'b':
         display(f'Congratulations! You are part of the RedBulldogs', target='output')
-        document.getElementById("image").innerHTML = "Place the image tag here"
-    elif section == 'ruby' and grade_level == 8 or 9:
-        display(f'Congratulations! You are part of the Bluebears', target='output')
-        document.getElementById("image").innerHTML = "Place the image tag here"
-    elif section == 'ruby' and grade_level == 7 or 10:
-        display(f'Congratulations! You are part of the YellowTigerss', target='output')
-        document.getElementById("image").innerHTML = "Place the image tag here"
+        document.getElementById("image").innerHTML = "<img src='red bulldogs.jpg' height='300' width='300'>"
+    elif grade_level == 'f':
+        display(f'Congratulations! You are part of the BlueBears', target='output')
+        document.getElementById("image").innerHTML = "<img src='blue bears.jpg' height='300' width='300'>"
+    elif grade_level == 'e':
+        display(f'Congratulations! You are part of the YellowTigers', target='output')
+        document.getElementById("image").innerHTML = "<img src='yellow tigers.jpg' height='300' width='300'>"
+    elif grade_level == 'c':
+        display(f'Congratulations! You are part of the RedBulldogs', target='output')
+        document.getElementById("image").innerHTML = "<img src='red bulldogs.jpg' height='300' width='300'>"
+    elif grade_level == 'g':
+        display(f'Congratulations! You are part of the BlueBears', target='output')
+        document.getElementById("image").innerHTML = "<img src='blue bears.jpg' height='300' width='300'>"
+    elif grade_level == 'h':
+        display(f'Congratulations! You are part of the YellowTigers', target='output')
+        document.getElementById("image").innerHTML = "<img src='yellow tigers.jpg' height='300' width='300'>"
     else: 
         display(f'Congratulations! You are part of the GreenHornets', target='output')
-        document.getElementById("image").innerHTML = "Place the image tag here"
+        document.getElementById("image").innerHTML = "<img src='green hornets.jpg' height='300' width='300'>"
+   
    
